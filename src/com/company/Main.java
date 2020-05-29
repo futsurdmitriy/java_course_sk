e642dcebe2a94600166420d818447b026633a7ce
fuda <futsurdmitriy@gmail.com>
2020-05-29 04:16:42 +0300

    Completed fourth part of task_1.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,9 +8,31 @@ package com.company;
  * Copyright Futsur Dmitriy
  */
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        List<String> lines = Files.readAllLines(Paths.get(System.getProperty(
+                "user.dir") + "/src/_harryPotter_1.txt"),
+                StandardCharsets.UTF_8);
+        String content = lines.toString();
+        String[] words = content.replaceAll("[\\P{L}]+", " ").trim().split(
+                "\\s+");
+        Arrays.sort(words);
+        String[] unique =
+                Arrays.stream(words).distinct().toArray(String[]::new);
+        for (String s : unique) {
+            System.out.println(s);
+        }
 
     }
 }
+
+
