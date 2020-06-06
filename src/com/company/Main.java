ed3278181f2264e08fc4ba3367655c47ae4ae4f8
fuda <futsurdmitriy@gmail.com>
2020-06-06 17:24:56 +0300

    Completed "Java Class Creation" task.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,41 +1,92 @@
-package com.company;
-
 /*
- * Classname Main
+ * Class name :  Main
  *
- * Version info 2.1
+ * Version info 1.0
  *
- * Copyright Futsur Dmitriy
+ * 06-Jun-2020
+ *
+ * Copyright (c) Dmitriy Futsur
+ *
+ * Description:
+ * Java class creation
+    Create a class from the attached document according to the order in your group .  The class must contain
+    1. Constructor.
+    2. Getters/Setters.
+    3. 5 methods.
+    4. Override toString() method
+    5. Override hash() and equals() methods.
+
+    My geometric figure is Rectangular Box.
  */
+
+package com.company;
 
 public class Main {
 
+    /**
+     * Method that is default entry point of a program.
+     * @param args Default argument for main method.
+     */
     public static void main(String[] args) {
 
+        // Instantiate first RectangularBox object with constructor
+        // with default params.
         RectangularBox rectangleBox1 = new RectangularBox();
-        RectangularBox.outputResult("The surface area of rectangle box 1 " +
-                "equals: ", rectangleBox1.calculateSurfaceArea());
-        RectangularBox.outputResult("The diagonal length of rectangle box 1 " +
-                "equals: ",
-                rectangleBox1.calculateDiagonalLengthOfRectangularBox());
-        RectangularBox.outputResult("The squared diagonal length of rectangle" +
-                " box 1 equals: ",
-                rectangleBox1.calculateSquaredDiagonalLengthOfRectangularBox());
-        RectangularBox.outputResult("The diagonal of rectangle box surface 1 " +
-                "equals: ",
-                rectangleBox1.calculateDiagonalOfARectangleBoxSurface());
 
-        RectangularBox rectangleBox2 = new RectangularBox(3, 4, 6);
-        RectangularBox.outputResult("The surface area of rectangle box 2 " +
-                "equals: ", rectangleBox2.calculateSurfaceArea());
-        RectangularBox.outputResult("The diagonal length of rectangle box 2 " +
-                        "equals: ",
+        System.out.println("Result of \"toString\" overridden method " +
+                "for rectangle box 1 for equals: " +
+                rectangleBox1.toString());
+        System.out.println("Result of \"hash\" overridden method " +
+                "for rectangle box 1 equals: " +
+                rectangleBox1.hashCode());
+
+        // Output methods results for first object
+        System.out.println("The surface area of rectangle box 1 " +
+                "equals: " + rectangleBox1.calculateSurfaceArea());
+        System.out.println("The diagonal length of rectangle box 1 " +
+                "equals: " +
+                rectangleBox1.calculateDiagonalLengthOfRectangularBox());
+        System.out.println("The squared diagonal length of rectangle" +
+                " box 1 equals: " +
+                rectangleBox1.calculateSquaredDiagonalLengthOfRectangularBox());
+        System.out.println("The diagonal of rectangle box 1 surface  " +
+                "equals: " +
+                rectangleBox1.calculateDiagonalOfARectangleBoxSurface());
+        System.out.println("The volume of rectangle box 1 " +
+                "equals: " +
+                rectangleBox1.calculateVolumeOfRectangularBox());
+
+        // Instantiate second RectangularBox object with constructor
+        // with default params.
+        RectangularBox rectangleBox2 =
+                new RectangularBox(3, 4, 6);
+
+        System.out.println("Result of \"toString\" overridden method " +
+                "for rectangle box 2 equals: " +
+                rectangleBox2.toString());
+        System.out.println("Result of \"hash\" overridden method " +
+                "for rectangle box 2 equals: " +
+                rectangleBox2.hashCode());
+
+        // Output methods results for first object
+        System.out.println("\nThe surface area of rectangle box 2 " +
+                "equals: " + rectangleBox2.calculateSurfaceArea());
+        System.out.println("The diagonal length of rectangle box 2 " +
+                        "equals: " +
                 rectangleBox2.calculateDiagonalLengthOfRectangularBox());
-        RectangularBox.outputResult("The squared diagonal length of rectangle" +
-                        " box 2 equals: ",
+        System.out.println("The squared diagonal length of rectangle" +
+                        " box 2 equals: " +
                 rectangleBox2.calculateSquaredDiagonalLengthOfRectangularBox());
-        RectangularBox.outputResult("The diagonal of rectangle box 2 surface " +
-                        "equals: ",
+        System.out.println("The diagonal of rectangle box 2 surface " +
+                        "equals: " +
                 rectangleBox2.calculateDiagonalOfARectangleBoxSurface());
+        System.out.println("The volume of rectangle box 2 " +
+                "equals: " +
+                rectangleBox2.calculateVolumeOfRectangularBox());
+
+        // Result of equals method
+        System.out.println("\nIs rectangular box 1 object equals" +
+                "rectangle box 2 : " +rectangleBox1.equals(rectangleBox2));
+
     }
 }
--- src/com/company/RectangularBox.java
+++ src/com/company/RectangularBox.java
@@ -1,18 +1,46 @@
+/*
+ * Class name :  RectangularBox
+ *
+ * Version info 1.0
+ *
+ * 06-Jun-2020
+ *
+ * Copyright (c) Dmitriy Futsur
+ *
+ * Description:
+ * Java class creation
+    Create a class from the attached document according to the order in your group .  The class must contain
+    1. Constructor.
+    2. Getters/Setters.
+    3. 5 methods.
+    4. Override toString() method
+    5. Override hash() and equals() methods.
+
+    My geometric figure is Rectangular Box.
+ */
+
 package com.company;
 
-/*
- * Classname RectangularBox
- *
- * Version info 2.1
- *
- * Copyright Futsur Dmitriy
- */
+import java.util.Objects;
 
 public class RectangularBox {
 
+    /**
+     * Length of rectangularBox
+     */
     private int length;
+
+    /**
+     * Width of rectangularBox
+     */
     private int width;
+
+    /**
+     * Height of rectangularBox
+     */
     private int height;
+
+    // 1. Constructor.
 
     // default constructor.
     // It will be created by JAVA
@@ -23,42 +51,125 @@ public class RectangularBox {
     //
     // }
 
+    /**
+     * Class constructor without parameters
+     */
     public RectangularBox() {
         this.setHeight(4);
         this.setWidth(6);
         this.setLength(5);
     }
 
+    /**
+     * Class constructor with parameters
+     * @param length Length that will be set to rectangularBox object
+     * @param width Width that will be set to rectangularBox object
+     * @param height Height that will be set to rectangularBox object
+     */
     public RectangularBox(int length, int width, int height) {
         this.length = length;
         this.width = width;
         this.height = height;
     }
 
+    // 2. Getters/Setters.
+
+    /**
+     * Getter for rectangularBox length field
+     * @return Returns length of rectangularBox object
+     */
     public int getLength() {
         return length;
     }
 
+    /**
+     * Setter for rectangularBox length field
+     * @param length Sets length of rectangularBox object
+     */
     public void setLength(int length) {
         this.length = length;
     }
 
+    /**
+     * Getter for rectangularBox width field
+     * @return Returns width of rectangularBox object
+     */
     public int getWidth() {
         return width;
     }
 
+    /**
+     * Setter for rectangularBox width field
+     * @param width Sets width of rectangularBox object
+     */
     public void setWidth(int width) {
         this.width = width;
     }
 
+    /**
+     * Getter for rectangularBox height field
+     * @return Returns height of rectangularBox object
+     */
     public int getHeight() {
         return height;
     }
 
+    /**
+     * Setter for rectangularBox height field
+     * @param height Sets height of rectangularBox object
+     */
     public void setHeight(int height) {
         this.height = height;
     }
 
+    // 4. Override toString() method
+
+    /**
+     * Method to get formatted rectangularBox object
+     * @return Returns info about rectangularBox object
+     */
+    @Override
+    public String toString() {
+        return "RectangularBox{" +
+                " length=" + length +
+                ", width=" + width +
+                ", height=" + height +
+                '}';
+    }
+
+    // 5. Override hash() and equals() methods.
+
+    /**
+     * Method to check whether object equals by value
+     * @param o Object as parameter
+     * @return Return boolean value as a result of comparison
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RectangularBox that = (RectangularBox) o;
+        return  getLength() == that.getLength() &&
+                getWidth() == that.getWidth() &&
+                getHeight() == that.getHeight();
+    }
+
+    /**
+     * Method to get object hash
+     * @return Returns rectangularBox object hash.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(getLength(), getWidth(), getHeight());
+    }
+
+    // 3. 5 methods.
+
+    /**
+     * Method that checks if rectangle has all fields set.
+     * If not return false.
+     * @return Returns boolean value that depends on rectangularBox fields.
+     */
     private boolean rectangleExists() {
         if (this.getHeight() == 0 ||
                 this.getWidth() == 0 ||
@@ -69,10 +180,16 @@ public class RectangularBox {
         }
     }
 
-    public int calculateSquaredDiagonalLengthOfRectangularBox() {
+    /**
+     * Method that calculates squared diagonal length of rectangularBox.
+     * You can see formula on
+     * https://ru.wikipedia.org/wiki/Прямоугольный_параллелепипед
+     * @return Returns calculated squared diagonal length of rectangularBox.
+     */
+    public double calculateSquaredDiagonalLengthOfRectangularBox() {
         if (this.rectangleExists()) {
-            return (int) (Math.pow(getLength(), 2) *
-                    Math.pow(getHeight(), 2) *
+            return  (Math.pow(getLength(), 2) +
+                    Math.pow(getHeight(), 2) +
                     Math.pow(getWidth(), 2));
         } else {
             System.out.println("Rectangular box is empty. " +
@@ -81,11 +198,22 @@ public class RectangularBox {
         }
     }
 
-    public int calculateDiagonalLengthOfRectangularBox() {
-        return (int)
-                Math.sqrt(this.calculateSquaredDiagonalLengthOfRectangularBox());
+    /**
+     * Method that calculates diagonal length of rectangularBox.
+     * You can see formula on
+     * https://ru.wikipedia.org/wiki/Прямоугольный_параллелепипед
+     * @return Returns calculated diagonal length of rectangularBox.
+     */
+    public double calculateDiagonalLengthOfRectangularBox() {
+        return Math.sqrt(this.calculateSquaredDiagonalLengthOfRectangularBox());
     }
 
+    /**
+     * Method that calculates surface area of rectangularBox.
+     * You can see formula on
+     * https://ru.wikipedia.org/wiki/Прямоугольный_параллелепипед
+     * @return Returns calculated surface area of rectangularBox.
+     */
     public int calculateSurfaceArea() {
         if (this.rectangleExists()) {
             return 2 * (getLength() * getWidth() +
@@ -98,9 +226,17 @@ public class RectangularBox {
         }
     }
 
-    public int calculateDiagonalOfARectangleBoxSurface() {
+    /**
+     * Calculates the diagonal of surface of rectangularBox by Pythagorean
+     * theorem. If the lengths of both a and b are known,
+     * then c can be calculated as c = √( a*a + b*b ), where a = height and
+     * b = length.
+     * URL https://en.wikipedia.org/wiki/Pythagorean_theorem
+     * @return Returns calculated diagonal length of a rectangularBox surface.
+     */
+    public double calculateDiagonalOfARectangleBoxSurface() {
         if (this.getHeight() != 0 && this.getLength() != 0) {
-            return (int) Math.sqrt(this.getHeight() * this.getLength());
+            return Math.sqrt(this.getHeight() * this.getLength());
         } else {
             System.out.println("Height and length of rectangle box is empty. " +
                     "Diagonal of surface cannot be calculated.");
@@ -108,7 +244,18 @@ public class RectangularBox {
         }
     }
 
-    public static void outputResult(String message, int variable) {
-        System.out.println(message + variable);
+    /**
+     * Method that calculates volume of a rectangularBox.
+     * @return Returns volume of rectangularBox.
+     */
+    public int calculateVolumeOfRectangularBox() {
+        if (this.rectangleExists()) {
+            return this.getLength() * this.getHeight() * this.getWidth();
+        } else {
+            System.out.println("Rectangular box is empty. " +
+                    "Surface area cannot be calculated.");
+            return 0;
+        }
     }
+
 }
