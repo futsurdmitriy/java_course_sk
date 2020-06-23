1d14437dfe37bdb5ca466ba79dbbbe13228d715b
fuda <futsurdmitriy@gmail.com>
2020-06-23 20:56:23 +0300

    Almost Done.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -22,7 +22,9 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -39,52 +41,41 @@ public class Main {
      */
     public static void main(String[] args) throws IOException {
 
-        // 1. Find in the net and download a text version of a novel about Harry
-        // Potter.  For instance,  "Harry Potter and the Sorcerer's
-        // Stone".
-        // Here we just set all the lines from file "_harryPotter_1.txt"
-        // to "lines" variable.
+        // 1.1 Parse the file harry.txt (see the attachment).
+        // Extract to a separate file all the logs from October 2019.
+
+
+        // 1.2. For each distinct word in the text calculate the number
+        // of occurrence.
+        HashMap<String, Integer> glossary = new HashMap<>();
+
+        // 1.3. Use RegEx.
+
+        // 1.4. Sort in the DESC mode by the number of occurrence.
+
+        // 1.5. Find  the first 20 pairs.
+
+        // 1.6  Find all the proper names
+
+        // 1.7.  Count them and arrange in alphabetic order.
+
+        // 1.8.   First 20 pairs and names write into to a file test.txt
+
+        // 1.9.  Create a fine header for the file
+
+        // 1.10  Use Java  Collections to demonstrate your experience (Map, List )
         List<String> lines = Files.readAllLines(Paths.get(System.getProperty(
                 "user.dir") + "/src/_harryPotter_1.txt"),
                 StandardCharsets.UTF_8);
 
-        // 2. Split the novel into an array of words.
-        // 3. Clean the words from a punctuation signs.
-        //
-        // To make it work we convert all the lines into one string
-        // variable "content". And then replacing replacing all
-        // symbols that is not words ([\P{L}]+) by spaces. After that we split
-        // content string by spaces thar we created to get array of words.
-        String content = lines.toString();
-        String[] words = content
-                .replaceAll("[\\P{L}]+", " ")
-                .trim()
-                .toLowerCase()
-                .split("\\s+");
+        List<String> words = new ArrayList<>(Arrays.asList(
+                lines.toString()
+                    .replaceAll("[\\P{L}]+", " ")
+                    .trim()
+                    .split("\\s+")
+        ));
 
-        // 4. Create another array containing distinct words.
-        // Here we will append to "uniqueWordsString" variable
-        // distinct words.
-        String uniqueWordsString = "";
-        for (int i = 0; i < words.length; i++) {
-            if (!uniqueWordsString.contains(words[i])){
-                uniqueWordsString += words[i] + " ";
-            }
-        }
-
-        // Splitting our long string by spaces
-        // to get array of distinct words.
-        String[] uniqueWords = uniqueWordsString
-                .trim()
-                .split("\\s+");
-
-        // 5. Order the words in alphabetic order.
-        Arrays.sort(uniqueWords);
-
-        // Finally we output all the words after our manipulations.
-        for (String s : uniqueWords) {
-            System.out.println(s);
-        }
+        System.out.println(words);
 
     }
 }
