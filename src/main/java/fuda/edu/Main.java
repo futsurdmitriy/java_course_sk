c4959a9b29e7ef9905cdc877d7907d1840c83cb1
fuda <futsurdmitriy@gmail.com>
2020-08-05 19:55:57 +0300

    Commit figure testing.

--- src/main/java/fuda/edu/Main.java
+++ src/main/java/fuda/edu/Main.java
@@ -2,8 +2,12 @@ package fuda.edu;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(PalindromeService.isValid("AbBa"));
-
+        RectangularBox rectangularBox = new RectangularBox(3,5,6);
+        System.out.println(rectangularBox.calculateDiagonalLengthOfRectangularBox());
+        System.out.println(rectangularBox.calculateDiagonalOfARectangleBoxSurface());
+        System.out.println(rectangularBox.calculateSquaredDiagonalLengthOfRectangularBox());
+        System.out.println(rectangularBox.calculateSurfaceArea());
+        System.out.println(rectangularBox.calculateVolumeOfRectangularBox());
         // 1) static method
         // 2) encapsulated logic to check palindrome
         // 3) logger for each method
---
+++ src/test/java/fuda/edu/RectangularBoxTest.java
@@ -0,0 +1,7 @@
+package fuda.edu;
+
+import static org.junit.Assert.*;
+
+public class RectangularBoxTest {
+    RectangularBox rectangularBox = new RectangularBox();
+}
